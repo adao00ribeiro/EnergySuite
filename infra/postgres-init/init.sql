82de35cbbe79b87bbaa78fddede8d1ed3bbc5a7e
Adão Eduardo Ribeiro <adao-eduardo@hotmail.com>
2026-08-25 13:57:41 -0300

    chore: migrate Airflow backend to PostgreSQL and update MLflow dependencies and CORS configuration

--- infra/postgres-init/init.sql
+++ infra/postgres-init/init.sql
@@ -3,3 +3,7 @@ WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'risk_db')\gexec
 
 SELECT 'CREATE DATABASE mlflow_db'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'mlflow_db')\gexec
+
+SELECT 'CREATE DATABASE airflow_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'airflow_db')\gexec
+
